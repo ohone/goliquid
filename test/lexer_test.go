46e8262e80bdf5a093b99e8ebdb3b5651d43243c
eoghan <eoghan.firth@gmail.com>
2021-08-06 19:33:11 +0100

    added test case

--- test/lexer_test.go
+++ test/lexer_test.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/ohone/goliquid/templater"
+	"github.com/ohone/goliquid/lexer"
 )
 
 func TestLex(t *testing.T) {
-	b := templater.Lex("name", "string")
+	b := lexer.Lex("name", "stringlol")
 	eme := b.NextLexeme()
+	fmt.Printf(eme.Token)
+	if eme.Templatable {
+		t.Error()
+	}
+}
+func TestLexTemplateable(t *testing.T) {
+	b := lexer.Lex("name", "{{hello}}")
+	eme := b.NextLexeme()
+	fmt.Printf(eme.Token)
+	eme2 := b.NextLexeme()
+	fmt.Printf(eme2.Token)
+	eme3 := b.NextLexeme()
+	fmt.Printf(eme3.Token)
+
 	if eme.Templatable {
 		t.Error()
 	}
